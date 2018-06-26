b322c692e2541a09fadbc76498701c7b22a18295
沈夏 <chenxia@chenxiadeMacBook-Pro.local>
2018-06-26 19:17:12 +0800

    add ISboxplot

---
+++ R/ISboxplot.R
@@ -0,0 +1,23 @@
+#' @title ISboxplot
+#' @description a function can generate internal standard boxplot
+#' @author Shine Shen
+#' \email{qq951633542@@163.com}
+#' @param sample a numeric object show the number of sample.
+#' @return  All the results can be got form other functions and instruction.
+#' @export
+#' @examples
+#' \donttest{
+#' ##---- Be sure the format of data and sample.info is correct!! ----
+#' }
+ISboxplot <- function(sample = NULL){
+  require(ggpubr);
+  data <- read.csv("IS data.csv")
+  mz <- c(rep(c(273.0762,271.0616),sample))
+  data1 <- stack(data)
+  data1 <- cbind(data1,mz)
+  png(filename = "boxplot of IS.png", width = 800, height = 600,res = 56*2)
+  gplot <- ggboxplot(data = data1,x="mz",y="scale(values)",color = "mz",
+                  palette = "jco",add = "jitter",xlab = "Internal Standard",
+                  ylab = "Peak Area",title = "Boxplot of Internal Standard")
+  dev.off()
+}
