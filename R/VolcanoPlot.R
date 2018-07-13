7e665ea2df5619123f23134301a4ad97d7ff6553
沈夏 <chenxia@chenxiadeMacBook-Pro.local>
2018-07-13 18:13:22 +0800

    optimize vol label

--- R/VolcanoPlot.R
+++ R/VolcanoPlot.R
@@ -83,8 +83,8 @@ volcano <- function(data = NULL,sample.info = NULL,p.cutoff = 0,
   volc1 <- ggplot(vol, aes(x = log2(fc), y = -log10(p)))+
     geom_point(aes(color = Significant)) +
     scale_color_manual(values = c("green", "grey","red")) +
-    annotate("text",x=xlim[2]-1,y=quantile(-log10(p),0.9),label=group2)+
-    annotate("text",x=xlim[2]-1.5,y=quantile(-log10(p),0.9),label=group1)+
+    annotate("text",x=xlim[2]-1,y=quantile(-log10(p),0.9999),label=group2)+
+    annotate("text",x=xlim[2]-1.5,y=quantile(-log10(p),0.9999),label=group1)+
     theme_bw(base_size = 16) +
     geom_vline(xintercept=c(-1,1),
                lty=4,col="orange",lwd=1)+ # 在x轴-1.5与1.5的位置画两根竖线
@@ -113,8 +113,8 @@ volcano <- function(data = NULL,sample.info = NULL,p.cutoff = 0,
     volc2 <- ggplot(vol, aes(x = log2(fc), y = -log10(p)))+
       geom_point(aes(color = Significant)) +
       scale_color_manual(values = c("green", "grey","red")) +
-      annotate("text",x=xlim[2]-1,y=quantile(-log10(p),0.9),label=group2)+
-      annotate("text",x=xlim[2]-1.5,y=quantile(-log10(p),0.9),label=group1)+
+      annotate("text",x=xlim[2]-1,y=quantile(-log10(p),0.9999),label=group2)+
+      annotate("text",x=xlim[2]-1.5,y=quantile(-log10(p),0.9999),label=group1)+
       theme_bw(base_size = 16) +
       geom_vline(xintercept= 0,
                  lty=4,col="orange",lwd=1)+ # 在x轴-1.5与1.5的位置画两根竖线
