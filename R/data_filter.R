42e3336f918185f9ccc3d8d6cf6ccce1c4e9f59b
沈夏 <chenxia@chenxiadeMBP.lan>
2018-07-28 20:48:21 +0800

    fix bug

--- R/data_filter.R
+++ R/data_filter.R
@@ -4,7 +4,6 @@
 #' and vip less than 1 or any value you can choose
 #' @author Shine Shen
 #' \email{qq951633542@@163.com}
-#' @param data data.
 #' @param p p value
 #' @param comp1 component 1 in PLSDA
 #' @param comp2 component 2 in PLSDA
@@ -16,13 +15,11 @@
 #' \donttest{
 #' ##---- Be sure the format of data and sample.info is correct!! ----
 #' }
-data_filter <- function(data=NULL,p=0.05,comp1=1,comp2=1,
+data_filter <- function(p=0.05,comp1=1,comp2=1,
                         fc_toplimit=1.2,fc_lowerlimit=0.8){
   require(data.table)
   data <- fread("data_pfc_vip.csv")
   data <- setDF(data)
-        idx.p<-which(data$p>p)
-        data<-data[-idx.p,]
         idx.vip1<-which(data$`comp 1`<comp1)
         data<-data[-idx.vip1,]
         idx.vip2<-which(data$`comp 2`<comp2)
@@ -31,6 +28,8 @@ data_filter <- function(data=NULL,p=0.05,comp1=1,comp2=1,
         data1<-data[idx.fc1,]
         idx.fc2<-which(data$fc>fc_toplimit)
         data2<-data[idx.fc2,]
-        data_final<-rbind(data1,data2)
+        data_mid<-rbind(data1,data2)
+        idx.p<-which(data_mid$p>p)
+        data_final<-data_mid[-idx.p,]
         write.csv(data_final,"data final.csv",row.names = FALSE)
   }
