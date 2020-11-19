d13c44601accf7e420b9369560a3e996c347f906
shineshen007 <qq951633542@163.com>
2020-11-19 19:31:58 +0800

    up

--- R/boxplot_Shine.R
+++ R/boxplot_Shine.R
@@ -2,20 +2,19 @@
 #' @description a function to draw BoxPlot
 #' @author Shine Shen
 #' \email{qq951633542@@163.com}
-#' @param ppt_width default is 9
-#' @param ngroup number of grouo
 #' @param levels order of group
 #' @param font_size font_size
+#' @param height 7
+#' @param width 9
 #' @return  All the results can be got form other functions and instruction.
 #' @export
 #' @examples
 #' \donttest{
 #' ##---- Be sure the format of data and sample.info is correct!! ----
 #' }
-Boxplot_Shine <- function(ppt_width = 9,
-                          ngroup = 3,
-                          font_size = 20,
-                          levels =c ("NU","HU","Gout")
+Boxplot_Shine <- function(font_size = 20,
+                          levels =c ("NU","HU","Gout"),
+                          height=7,width=9
 
 ){
   pacman::p_load(ggpubr,ggsci)
@@ -34,7 +33,7 @@ Boxplot_Shine <- function(ppt_width = 9,
           axis.title.y = element_text(size = font_size)
     )+
     stat_compare_means(aes(group = group),label = "p.signif", label.x = 1.5)+
-    ggsave('boxplot.pdf',height=7,width=9)
+    ggsave('boxplot.pdf',height=height,width=height)
   save(s,file = 'boxplot.Rda')
   #export::graph2ppt(x=s,file='boxplot.pptx',height=7,width=ppt_width)
 }
--- R/violin.R
+++ R/violin.R
@@ -2,18 +2,20 @@
 #' @description a function to draw BoxPlot
 #' @author Shine Shen
 #' \email{qq951633542@@163.com}
-#' @param ppt_width default is 9
 #' @param levels order of group
 #' @param font_size font_size
+#' @param height 7
+#' @param width 9
 #' @return  All the results can be got form other functions and instruction.
 #' @export
 #' @examples
 #' \donttest{
 #' ##---- Be sure the format of data and sample.info is correct!! ----
 #' }
-Violinplot_Shine <- function(ppt_width = 9,
+Violinplot_Shine <- function(
                           font_size = 20,
-                          levels = c("Normal","CHD")
+                          levels = c("Normal","CHD"),
+                          height=7,width=9
 
 ){
   pacman::p_load(ggplot2,ggsci)
@@ -31,9 +33,10 @@ Violinplot_Shine <- function(ppt_width = 9,
           axis.text.y = element_text(size = font_size),
           axis.title.x = element_text(size = 0),#the font size of axis title
           axis.title.y = element_text(size = font_size)
-    )
+    )+
+    stat_compare_means(aes(group = group),label = "p.signif", label.x = 1.5)+
+  ggsave('violinplot.pdf',height=height,width=height)
   save(s,file = 'violinplot.Rda')
-  ggsave('boxplot.pdf',height=7,width=9)
   #export::graph2ppt(x=s,file='violinplot.pptx',height=12,width=ppt_width)
 }
 
