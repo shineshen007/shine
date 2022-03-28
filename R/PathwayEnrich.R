d32f78aefe5da738b6639275b7302e041166f943
shineshen007 <qq951633542@163.com>
2022-03-28 16:06:28 +0800

    p

--- R/PathwayEnrich.R
+++ R/PathwayEnrich.R
@@ -21,7 +21,7 @@ PathwayEnrich <- function(specias_pathway_database= hsa.kegg.pathway,
                           seed = FALSE,
                           grade1_2 = FALSE
 ){
-  pacman::p_load(ggplotify,readr,data.table,export,magrittr)
+  pacman::p_load(ggplotify,readr,crayon,export,magrittr)
   if(All_ID){
     data <- read_csv(data_name)
     if(!file.exists('PathwayEnrich_All_ID')){
@@ -72,51 +72,57 @@ PathwayEnrich <- function(specias_pathway_database= hsa.kegg.pathway,
     colnames(info) <- c("Pathway.length", "Overlap")
 
     info <- data.frame(PBF, info, stringsAsFactors = FALSE)
-    setwd('PathwayEnrich_All_ID')
+
     info <- info[order(info[,1]),]
-    write.csv(info,'pathway_results.csv')
+    write.csv(info,'PathwayEnrich_All_ID/pathway_results.csv')
     #o('pathway_impact_76_pathway_hsa.all.ID.csv')
     sig_path <- dplyr::filter(info,q.value<0.05)
-    ip <- lapply(specias_pathway_database, function(x){
-      intersect(x,mid)
-    })
+    if(nrow(sig_path)==0){
+      cat(red('No significant pathway in enrichment analysis.\n'))
+    }else{
+      ip <- lapply(specias_pathway_database, function(x){
+        intersect(x,mid)
+      })
 
-    for (i in setdiff(names(specias_pathway_database),rownames(sig_path))) {
-      ip[[i]]=NULL
+      for (i in setdiff(names(specias_pathway_database),rownames(sig_path))) {
+        ip[[i]]=NULL
+      }
+
+      for (i in 1:length(ip)) {
+        ip[[i]] <- data[match(ip[[i]],data$ID),]
+      }
+      sheetnames <- nchar(names(ip))
+      if(max(sheetnames)>31){
+        names(ip)[which(sheetnames>31)]=substr(names(ip)[which(sheetnames>31)],1,30)
+      }
+      setwd('PathwayEnrich_All_ID')
+      openxlsx::write.xlsx(ip,'Id_pathway.xlsx',overwrite = T)
+      #
+      data <- read.csv("pathway_results.csv")
+      colnames(data)[3] <- 'p'
+      row=sum(data$p<0.05)+3
+      data <- data[1:row,]
+      group <- ifelse(data$p < 0.05,"sig", "not sig")
+      pb <- ggplot2::ggplot(data,ggplot2::aes(reorder(X,-p),-log10(p)))+##-p control the order
+        ggplot2::geom_bar(aes(fill=group),stat = "identity",position="dodge",width=0.8)+
+        scale_fill_manual(values = color)+
+        ggplot2::theme(panel.grid.major =element_blank(), panel.grid.minor = element_blank(),#remove ggplot2 background
+                       panel.background = element_blank(),axis.line = element_line(colour = "black"),
+                       legend.position = "none",axis.text.y = element_text(size = font_size),
+                       axis.text.x = element_text(size = font_size),#the font size of axis
+                       axis.title.x = element_text(size = font_size),#the font size of axis title
+                       axis.title.y = element_text(size = font_size))+
+        ggplot2::geom_hline(yintercept= 1.30103,#draw a line at p=0.05
+                            lty=4,col="grey21",lwd=1)+
+        ggplot2::labs(title="Pathway Enrichment Analysis")+
+        ggplot2::coord_flip()+
+        ggplot2::xlab('Pathway')
+      save(pb,file = 'barplot.Rda')
+      export::graph2ppt(x=pb,file='pathway.pptx',height=7,width=9)
+      dev.off()
+      setwd('..//')
     }
 
-    for (i in 1:length(ip)) {
-      ip[[i]] <- data[match(ip[[i]],data$ID),]
-    }
-    sheetnames <- nchar(names(ip))
-    if(max(sheetnames)>31){
-      names(ip)[which(sheetnames>31)]=substr(names(ip)[which(sheetnames>31)],1,30)
-    }
-    openxlsx::write.xlsx(ip,'Id_pathway.xlsx',overwrite = T)
-    #
-    data <- read.csv("pathway_results.csv")
-    colnames(data)[3] <- 'p'
-    row=sum(data$p<0.05)+3
-    data <- data[1:row,]
-    group <- ifelse(data$p < 0.05,"sig", "not sig")
-    pb <- ggplot2::ggplot(data,ggplot2::aes(reorder(X,-p),-log10(p)))+##-p control the order
-      ggplot2::geom_bar(aes(fill=group),stat = "identity",position="dodge",width=0.8)+
-      scale_fill_manual(values = color)+
-      ggplot2::theme(panel.grid.major =element_blank(), panel.grid.minor = element_blank(),#remove ggplot2 background
-                     panel.background = element_blank(),axis.line = element_line(colour = "black"),
-                     legend.position = "none",axis.text.y = element_text(size = font_size),
-                     axis.text.x = element_text(size = font_size),#the font size of axis
-                     axis.title.x = element_text(size = font_size),#the font size of axis title
-                     axis.title.y = element_text(size = font_size))+
-      ggplot2::geom_hline(yintercept= 1.30103,#draw a line at p=0.05
-                          lty=4,col="grey21",lwd=1)+
-      ggplot2::labs(title="Pathway Enrichment Analysis")+
-      ggplot2::coord_flip()+
-      ggplot2::xlab('Pathway')
-    save(pb,file = 'barplot.Rda')
-    export::graph2ppt(x=pb,file='pathway.pptx',height=7,width=9)
-    dev.off()
-    setwd('..//')
   }
   #####
   if(seed){
@@ -170,54 +176,58 @@ PathwayEnrich <- function(specias_pathway_database= hsa.kegg.pathway,
     colnames(info) <- c("Pathway.length", "Overlap")
 
     info <- data.frame(PBF, info, stringsAsFactors = FALSE)
-    setwd('PathwayEnrich_seed')
+
     info <- info[order(info[,1]),]
-    write.csv(info,'pathway_results.csv')
+    write.csv(info,'PathwayEnrich_seed/pathway_results.csv')
     #o('pathway_impact_76_pathway_hsa.all.ID.csv')
     ##id match to pathway
     #compound_db <- read_csv('/Users/shineshen/Downloads/demo/kegg/old/kegg_compound_unique.csv')
     sig_path <- dplyr::filter(info,q.value<0.05)
-    ip <- lapply(specias_pathway_database, function(x){
-      intersect(x,mid)
-    })
+    if(nrow(sig_path)==0){
+      cat(red('No significant pathway in enrichment analysis.\n'))
+    }else{
+      ip <- lapply(specias_pathway_database, function(x){
+        intersect(x,mid)
+      })
 
-    for (i in setdiff(names(specias_pathway_database),rownames(sig_path))) {
-      ip[[i]]=NULL
-    }
+      for (i in setdiff(names(specias_pathway_database),rownames(sig_path))) {
+        ip[[i]]=NULL
+      }
 
-    for (i in 1:length(ip)) {
-      ip[[i]] <- data[match(ip[[i]],data$ID),]
-    }
-    sheetnames <- nchar(names(ip))
-    if(max(sheetnames)>31){
-      names(ip)[which(sheetnames>31)]=substr(names(ip)[which(sheetnames>31)],1,30)
-    }
-    openxlsx::write.xlsx(ip,'Id_pathway.xlsx',overwrite = T)
-    #
-    data <- read.csv("pathway_results.csv")
-    colnames(data)[3] <- 'p'
-    row=sum(data$p<0.05)+3
-    data <- data[1:row,]
-    group <- ifelse(data$p < 0.05,"sig", "not sig")
-    pb <- ggplot2::ggplot(data,ggplot2::aes(reorder(X,-p),-log10(p)))+##-p control the order
-      ggplot2::geom_bar(aes(fill=group),stat = "identity",position="dodge",width=0.8)+
-      scale_fill_manual(values = color)+
-      ggplot2::theme(panel.grid.major =element_blank(), panel.grid.minor = element_blank(),#remove ggplot2 background
-                     panel.background = element_blank(),axis.line = element_line(colour = "black"),
-                     legend.position = "none",axis.text.y = element_text(size = font_size),
-                     axis.text.x = element_text(size = font_size),#the font size of axis
-                     axis.title.x = element_text(size = font_size),#the font size of axis title
-                     axis.title.y = element_text(size = font_size))+
-      ggplot2::geom_hline(yintercept= 1.30103,#draw a line at p=0.05
-                          lty=4,col="grey21",lwd=1)+
-      ggplot2::labs(title="Pathway Enrichment Analysis")+
-      ggplot2::coord_flip()+
-      ggplot2::xlab('Pathway')
-    save(pb,file = 'barplot.Rda')
-    export::graph2ppt(x=pb,file='pathway.pptx',height=7,width=9)
-    dev.off()
-    setwd('..//')
-  }
+      for (i in 1:length(ip)) {
+        ip[[i]] <- data[match(ip[[i]],data$ID),]
+      }
+      sheetnames <- nchar(names(ip))
+      if(max(sheetnames)>31){
+        names(ip)[which(sheetnames>31)]=substr(names(ip)[which(sheetnames>31)],1,30)
+      }
+      setwd('PathwayEnrich_seed')
+      openxlsx::write.xlsx(ip,'Id_pathway.xlsx',overwrite = T)
+      #
+      data <- read.csv("pathway_results.csv")
+      colnames(data)[3] <- 'p'
+      row=sum(data$p<0.05)+3
+      data <- data[1:row,]
+      group <- ifelse(data$p < 0.05,"sig", "not sig")
+      pb <- ggplot2::ggplot(data,ggplot2::aes(reorder(X,-p),-log10(p)))+##-p control the order
+        ggplot2::geom_bar(aes(fill=group),stat = "identity",position="dodge",width=0.8)+
+        scale_fill_manual(values = color)+
+        ggplot2::theme(panel.grid.major =element_blank(), panel.grid.minor = element_blank(),#remove ggplot2 background
+                       panel.background = element_blank(),axis.line = element_line(colour = "black"),
+                       legend.position = "none",axis.text.y = element_text(size = font_size),
+                       axis.text.x = element_text(size = font_size),#the font size of axis
+                       axis.title.x = element_text(size = font_size),#the font size of axis title
+                       axis.title.y = element_text(size = font_size))+
+        ggplot2::geom_hline(yintercept= 1.30103,#draw a line at p=0.05
+                            lty=4,col="grey21",lwd=1)+
+        ggplot2::labs(title="Pathway Enrichment Analysis")+
+        ggplot2::coord_flip()+
+        ggplot2::xlab('Pathway')
+      save(pb,file = 'barplot.Rda')
+      export::graph2ppt(x=pb,file='pathway.pptx',height=7,width=9)
+      dev.off()
+      setwd('..//')
+    }}
   ###########
   if(grade1_2){
     dir.create('PathwayEnrich_grade1_2')
@@ -270,52 +280,56 @@ PathwayEnrich <- function(specias_pathway_database= hsa.kegg.pathway,
     colnames(info) <- c("Pathway.length", "Overlap")
 
     info <- data.frame(PBF, info, stringsAsFactors = FALSE)
-    setwd('PathwayEnrich_grade1_2')
+
     info <- info[order(info[,1]),]
-    write.csv(info,'pathway_results.csv')
+    write.csv(info,'PathwayEnrich_grade1_2/pathway_results.csv')
     #o('pathway_impact_76_pathway_hsa.all.ID.csv')
     sig_path <- dplyr::filter(info,q.value<0.05)
-    ip <- lapply(specias_pathway_database, function(x){
-      intersect(x,mid)
-    })
+    if(nrow(sig_path)==0){
+      cat(red('No significant pathway in enrichment analysis.\n'))
+    }else{
+      ip <- lapply(specias_pathway_database, function(x){
+        intersect(x,mid)
+      })
 
-    for (i in setdiff(names(specias_pathway_database),rownames(sig_path))) {
-      ip[[i]]=NULL
-    }
+      for (i in setdiff(names(specias_pathway_database),rownames(sig_path))) {
+        ip[[i]]=NULL
+      }
 
-    for (i in 1:length(ip)) {
-      ip[[i]] <- data[match(ip[[i]],data$ID),]
+      for (i in 1:length(ip)) {
+        ip[[i]] <- data[match(ip[[i]],data$ID),]
+      }
+      sheetnames <- nchar(names(ip))
+      if(max(sheetnames)>31){
+        names(ip)[which(sheetnames>31)]=substr(names(ip)[which(sheetnames>31)],1,30)
+      }
+      setwd('PathwayEnrich_grade1_2')
+      openxlsx::write.xlsx(ip,'Id_pathway.xlsx',overwrite = T)
+      #
+      data <- read.csv("pathway_results.csv")
+      colnames(data)[3] <- 'p'
+      row=sum(data$p<0.05)+3
+      data <- data[1:row,]
+      group <- ifelse(data$p < 0.05,"sig", "not sig")
+      pb <- ggplot2::ggplot(data,ggplot2::aes(reorder(X,-p),-log10(p)))+##-p control the order
+        ggplot2::geom_bar(aes(fill=group),stat = "identity",position="dodge",width=0.8)+
+        scale_fill_manual(values = color)+
+        ggplot2::theme(panel.grid.major =element_blank(), panel.grid.minor = element_blank(),#remove ggplot2 background
+                       panel.background = element_blank(),axis.line = element_line(colour = "black"),
+                       legend.position = "none",axis.text.y = element_text(size = font_size),
+                       axis.text.x = element_text(size = font_size),#the font size of axis
+                       axis.title.x = element_text(size = font_size),#the font size of axis title
+                       axis.title.y = element_text(size = font_size))+
+        ggplot2::geom_hline(yintercept= 1.30103,#draw a line at p=0.05
+                            lty=4,col="grey21",lwd=1)+
+        ggplot2::labs(title="Pathway Enrichment Analysis")+
+        ggplot2::coord_flip()+
+        ggplot2::xlab('Pathway')
+      save(pb,file = 'barplot.Rda')
+      export::graph2ppt(x=pb,file='pathway.pptx',height=7,width=9)
+      dev.off()
+      setwd('..//')
     }
-    sheetnames <- nchar(names(ip))
-    if(max(sheetnames)>31){
-      names(ip)[which(sheetnames>31)]=substr(names(ip)[which(sheetnames>31)],1,30)
-    }
-    openxlsx::write.xlsx(ip,'Id_pathway.xlsx',overwrite = T)
-    #
-    data <- read.csv("pathway_results.csv")
-    colnames(data)[3] <- 'p'
-    row=sum(data$p<0.05)+3
-    data <- data[1:row,]
-    group <- ifelse(data$p < 0.05,"sig", "not sig")
-    pb <- ggplot2::ggplot(data,ggplot2::aes(reorder(X,-p),-log10(p)))+##-p control the order
-      ggplot2::geom_bar(aes(fill=group),stat = "identity",position="dodge",width=0.8)+
-      scale_fill_manual(values = color)+
-      ggplot2::theme(panel.grid.major =element_blank(), panel.grid.minor = element_blank(),#remove ggplot2 background
-                     panel.background = element_blank(),axis.line = element_line(colour = "black"),
-                     legend.position = "none",axis.text.y = element_text(size = font_size),
-                     axis.text.x = element_text(size = font_size),#the font size of axis
-                     axis.title.x = element_text(size = font_size),#the font size of axis title
-                     axis.title.y = element_text(size = font_size))+
-      ggplot2::geom_hline(yintercept= 1.30103,#draw a line at p=0.05
-                          lty=4,col="grey21",lwd=1)+
-      ggplot2::labs(title="Pathway Enrichment Analysis")+
-      ggplot2::coord_flip()+
-      ggplot2::xlab('Pathway')
-    save(pb,file = 'barplot.Rda')
-    export::graph2ppt(x=pb,file='pathway.pptx',height=7,width=9)
-    dev.off()
-    setwd('..//')
   }
-
 }
 
